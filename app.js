142adc4493c9b46d9fffd1a6322e796aebe99974
Matthew Parks II <Matthew.B.Parks@pacecenter.org>
2023-12-20 15:42:19 -0500

    connected the backend to the front end

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+async function getItems() {
+  try {
+    const response = await fetch("http://localhost:3000/items");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data); // This will log the response from your API
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+  }
+}
 
+getItems();
--- server.js
+++ server.js
@@ -1,45 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
 // connecting to database
 const { CosmosClient } = require("@azure/cosmos");
 
 const endpoint = "https://wishlistdb.documents.azure.com:443/";
-const key = "pqylSI564bqFcK8eDIHnOpdNroD19NRGrtn5JbWMDaEF6bFTFX9jLLX2I61EAIVv8IPXNvY0CiiBACDbZJHVog==";
+const key =
+  "pqylSI564bqFcK8eDIHnOpdNroD19NRGrtn5JbWMDaEF6bFTFX9jLLX2I61EAIVv8IPXNvY0CiiBACDbZJHVog==";
 
 const client = new CosmosClient({ endpoint, key });
-
 
 const database = client.database("ToDoList");
 const container = database.container("Items");
 
 async function dbQuery() {
-
+  try {
     // querying the database
-  const query = "SELECT * FROM c";
-  const { resources } = await container.items.query(query).fetchAll();
+    const query = "SELECT * FROM c";
+    const { resources } = await container.items.query(query).fetchAll();
 
-  console.log(resources);
+    // console.log("Database query result:", resources);
+    return resources;
+  } catch (error) {
+    console.error("Error fetching from database:", error);
+    throw error;
+  }
 }
-
-dbQuery().catch((error) => {
-  console.error(error);
-});
 
 async function dbInsert() {
-
+  try {
     // Inserting into the database
     const websiteList = [
-        // insert in this formmat  { id: "4", name: "Urban Outfitters", url: "https://www.urbanoutfitters.com/" }
-       ];
-         for (const item of websiteList) {
-           await container.items.create(item); // Create each item with unique ID
-        }
-}
-dbInsert().catch((error) => {
+      // insert in this formmat  { id: "4", name: "Urban Outfitters", url: "https://www.urbanoutfitters.com/" }
+    ];
+    for (const item of websiteList) {
+      await container.items.create(item); // Create each item with unique ID
+    }
+  } catch (error) {
     console.error(error);
-  });
-  
+    return [];
+  }
+}
+
+app.use(cors());
+
+app.get("/items", async (req, res) => {
+  const items = await dbQuery();
+  //  console.log("Items fetched:", items);
+
+  res.json(items);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
