7ed90678f53205850fb1c264e77c35ba38d2a1e9
Matthew Parks II <Matthew.B.Parks@pacecenter.org>
2023-12-21 12:10:14 -0500

    frontend is now displaying the items

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ async function getItems() {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data); // This will log the response from your API
+    return data; // This will log the response from your API
   } catch (error) {
     console.error("Error fetching data:", error.message);
   }
 }
 
-getItems();
+// Function to update the DOM with fetched items
+async function updateDOMWithItems() {
+  const items = await getItems();
+  if (items && items.length > 0) {
+    const container = document.getElementById("website-list");
+    items.forEach((item) => {
+      var li = document.createElement("li");
+      li.textContent = item.name + item.url; // Assuming 'item' is a string
+      container.appendChild(li);
+    });
+  }
+}
+
+// Call the function when the script loads
+updateDOMWithItems();
